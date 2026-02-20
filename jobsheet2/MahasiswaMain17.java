6387712c6aedf967c998bb57abb9842330dd2885
byannaja <byank2314@gmail.com>
2026-02-20 10:57:27 +0700

    Percobaan 2

---
+++ jobsheet2/MahasiswaMain17.java
@@ -0,0 +1,17 @@
+package jobsheet2;
+
+public class MahasiswaMain17 {
+    public static void main(String[] args) {
+        Mahasiswa17 mhs1 = new Mahasiswa17();
+    mhs1.nama = "Muhammad Ali Farhan";
+    mhs1.nim = "24263764254";
+    mhs1.kelas = "SI 2J";
+    mhs1.ipk = 3.55;
+    
+    mhs1.tampilkanInformasi();
+    mhs1.ubahKelas("SI 2K");
+    mhs1.updateIpk(3.60);
+    mhs1.tampilkanInformasi();
+    }
+    
+}
