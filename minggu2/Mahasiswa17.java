668fe72a466d758a078cf98e408eb2f3f0ba930b
byannaja <byannaja@gmail.com>
2026-03-02 17:10:54 +0000

    Mahasiswa17.java

---
+++ minggu2/Mahasiswa17.java
@@ -0,0 +1,46 @@
+package jobsheet2;
+
+public class Mahasiswa17 {
+String nama, nim, kelas;
+double ipk;
+
+public Mahasiswa17(){
+    
+}
+
+public Mahasiswa17(String nm, String nim, String kls, double ipk){
+nama = nm;
+this.nim = nim;
+kelas = kls;
+this.ipk = ipk;
+}
+void tampilkanInformasi() {
+    System.out.println ("Nama : "+ nama);
+    System.out.println("NIM : "+ nim);
+    System.out.println("Kelas : "+ kelas);
+    System.out.println("IPK : "+ ipk);
+}
+void ubahKelas(String kelasBaru){
+    kelas = kelasBaru;
+}
+void updateIpk(double ipkBaru){
+    ipk = ipkBaru;
+}
+String nilaiKinerja(){
+    if (ipk < 0.0 || ipk > 4.0){
+        return "IPK tidak valid. Harus antara 0.0 dan 4.0";
+    }
+    else if (ipk >= 3.5 ) {
+        return "Kinerja sangat baik";
+    }
+    else if (ipk >= 3.0){
+        return "Kinerja baik";
+    }
+    else if (ipk >= 2.0){
+        return "Kinerja cukup";
+    }
+    else {
+        return "Kinerja kurang";
+    }
+}
+}
