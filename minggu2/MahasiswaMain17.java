f2f69e696b968ff3c76413ddabcab4ef3b607d38
byannaja <byannaja@gmail.com>
2026-03-02 11:48:39 +0000

    MahasiswaMain17.java

---
+++ minggu2/MahasiswaMain17.java
@@ -0,0 +1,24 @@
+package jobsheet2;
+
+public class MahasiswaMain17 {
+    public static void main(String[] args) {
+    Mahasiswa17 mhs1 = new Mahasiswa17();
+    mhs1.nama = "Muhammad Ali Farhan";
+    mhs1.nim = "24263764254";
+    mhs1.kelas = "SI 2J";
+    mhs1.ipk = 3.55;
+    
+    mhs1.tampilkanInformasi();
+    mhs1.ubahKelas("SI 2K");
+    mhs1.updateIpk(3.60);
+    mhs1.tampilkanInformasi();
+
+    Mahasiswa17 mhs2 = new Mahasiswa17("Nabila Anisa", "283748927", "TI 2L", 3.25);
+    mhs2.updateIpk(3.30);
+    mhs2.tampilkanInformasi();
+
+    Mahasiswa17 mhsByan = new Mahasiswa17("Byan", "2857645485", "TI 2B", 4.0);
+    mhsByan.tampilkanInformasi();
+    }
+    
+}
