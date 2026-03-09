9ee5e13fe87a61128b8b5b2effe8ccf10fc7a428
byannaja <byannaja@gmail.com>
2026-03-09 08:40:57 +0000

    Dosen17.java

---
+++ minggu3/Dosen17.java
@@ -0,0 +1,19 @@
+package jobsheet3;
+
+public class Dosen17 {
+    public String kode, nama;
+    public boolean jenisKelamin;
+    public int usia;
+
+public Dosen17(){
+
+}
+
+public Dosen17(String kode, String nama, boolean jenisKelamin, int usia){
+    this.kode = kode;
+    this.nama = nama;
+    this.jenisKelamin = jenisKelamin;
+    this.usia = usia;
+
+}
+}
