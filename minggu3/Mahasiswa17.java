2a1eb3e43476e119ed4eca4782140631ac669418
byannaja <byannaja@gmail.com>
2026-03-09 12:01:39 +0000

    Mahasiswa17.java

---
+++ minggu3/Mahasiswa17.java
@@ -0,0 +1,13 @@
+package jobsheet3;
+
+public class Mahasiswa17 {
+    public String nim, nama, kelas;
+    public float ipk;
+
+    void cetakInfo(){
+        System.out.println("Nama    : " + nama);
+        System.out.println("NIM     : "+ nim);
+        System.out.println("Kelas   : "+ kelas);
+        System.out.println("IPK     : "+ ipk);
+    }
+}
