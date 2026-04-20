7979858a7591261a55382aeee3fec53a03cf57fb
byannaja <byannaja@gmail.com>
2026-04-20 14:37:44 +0000

    MahasiswaBerprestasi17.java

---
+++ minggu7/MahasiswaBerprestasi17.java
@@ -0,0 +1,66 @@
+package jobsheet6;
+
+public class MahasiswaBerprestasi17 {
+    Mahasiswa17 listMhs[] = new Mahasiswa17[5];
+    int idx;
+
+    void tambah(Mahasiswa17 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa17 m : listMhs) {
+            m.tampilInformasi();
+            System.out.println("--------------------");
+        }
+    }
+
+    int sequentialSearching(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("data mahasiswa dengan IPK : " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("data " + x + "tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos) {
+        if (pos != -1) {
+            System.out.println("nim\t : " + listMhs[pos].nim);
+            System.out.println("nama\t : " + listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
+        }
+        
+    }
+    int findBinarySearch(double cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk) {
+                return (mid);
+            } else if (listMhs[mid].ipk > cari) {
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+}
