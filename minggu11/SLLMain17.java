e3937dae520d9f9ebf033c428b97e3e0dd10edc9
byannaja <byannaja@gmail.com>
2026-06-03 11:59:26 +0000

    percobaan 2

--- minggu11/SLLMain17.java
+++ minggu11/SLLMain17.java
@@ -1,4 +1,5 @@
 package minggu11;
+
 public class SLLMain17 {
     public static void main(String[] args) {
         SingleLinkedList17 sll = new SingleLinkedList17();
@@ -8,16 +9,21 @@ public class SLLMain17 {
         Mahasiswa17 mhs3 = new Mahasiswa17("22212202", "Cintia", "3C", 3.5);
         Mahasiswa17 mhs4 = new Mahasiswa17("21212203", "Dirga", "4O", 3.6);
 
-        sll.print();
-        System.out.println();
         sll.addFirst(mhs4);
-        sll.print();
-        System.out.println();
         sll.addLast(mhs1);
-        sll.print();
-        System.out.println();
         sll.insertAfter("Dirga", mhs3);
         sll.insertAt(2, mhs2);
+
+        System.out.println("data index 1: ");
+        sll.getData(1);
+        System.out.println("data mahasiswa an Bimon berada pada index: " + sll.indexOf("bimon"));
+        System.out.println("");
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+
+        sll.removeAt(0);
         sll.print();
     }
 }
